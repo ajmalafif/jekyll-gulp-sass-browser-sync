c5215b6636efa96ac4d37fcf0970cf119a6faebb
Ajmal Afif <ajmalafif@gmail.com>
2015-10-02 16:33:05 +0800

    Add Google  analytics

---
+++ _includes/google_analytics.js
@@ -0,0 +1,6 @@
+<script>
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', 'UA-15183705-4', 'auto');
+ga('send', 'pageview');
+</script>
+<script type="text/javascript">{% include analytics.js %}</script>
